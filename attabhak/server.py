6723cfa0f69e85b6e42572126a65914c0f8d4717
Thanathip Limna <boatkrap@gmail.com>
2025-10-02 23:20:41 +0700

    fix error

--- attabhak/server.py
+++ attabhak/server.py
@@ -1,18 +1,32 @@
-
 import datetime
 import logging
-
-
-
 import asyncio
+
+from . import monitor
+
 
 class Server:
     def __init__(self):
         self.running = False
         self.logger = logging.getLogger("attabhak.server")
+        self.monitor_tasks = []
+
+        self.config = dict()
 
     async def start(self):
-        logging.basicConfig(level=logging.DEBUG, format="%(asctime)s %(levelname)s %(name)s: %(message)s")
+        logging.basicConfig(
+            level=logging.DEBUG,
+            format="%(asctime)s %(levelname)s %(name)s: %(message)s",
+        )
+
+        self.monitor_tasks.append(
+            asyncio.create_task(
+                monitor.MQTTMonitor(self.config.get("broker_url")).monitor(
+                    self.config.get("topic")
+                )
+            )
+        )
+
         self.running = True
         self.logger.info(f"Server started")
         await self.run()
@@ -25,4 +39,3 @@ class Server:
     async def stop(self):
         self.running = False
         self.logger.info(f"Server stopped")
-
